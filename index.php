3639f981485b27fceb7bbb050d7ddb1e4619dd9a
Sven Imholz <91316910+SvenImholz@users.noreply.github.com>
2025-09-16 15:37:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$product = "Frietjes";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De beste <?php echo $product ?> van de hele wereld</title>
+</head>
+
+<body>
+    <h1>De beste <?php echo $product ?> van de hele wereld</h1>
+</body>
+
+</html>
