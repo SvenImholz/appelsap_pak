831c3208db54516666bf10ec729909b25f2792e9
Sven Imholz <91316910+SvenImholz@users.noreply.github.com>
2025-09-18 10:10:54 +0200

    Een index pagina toegevoegd

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lessen</title>
+    <style>
+        nav>a {
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <nav>
+        <a href="/src/les1">Les 1</a>
+        <a href="/src/les2_require">Les 2: require en require_once statement</a>
+    </nav>
+</body>
+
+</html>
